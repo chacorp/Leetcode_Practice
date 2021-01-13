0c097198e1618ecf6bec74cb232c36d342f31189
chacorp <sihun.cha@gmail.com>
2021-01-14 02:07:44 +0900

    Update TwoSum.py
    explanaton added

--- TwoSum.py
+++ TwoSum.py
@@ -5,8 +5,9 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        
+        # 무식하게 루프 돌기 >> 현재 위치와 나머지 모두를 하나씩 전부 비교
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i]+nums[j] == target:
                     return [i, j]
+
