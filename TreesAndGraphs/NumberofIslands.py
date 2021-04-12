f86912317c05d20a58b1493044f503a0ccac7a83
chacorp <68317086+chacorp@users.noreply.github.com>
2021-04-12 12:29:03 +0900

    Update NumberofIslands.py

--- TreesAndGraphs/NumberofIslands.py
+++ TreesAndGraphs/NumberofIslands.py
@@ -46,3 +46,34 @@ class Solution:
             t_c = no_z if t_c==0 and no_z!=0 else t_c
         return t_c
 ##################################################
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        row = len(grid)
+        col = len(grid[0])
+                     
+        t_c = 0
+                
+        for y in range(row):
+            for x in range(col):
+                # 현재 칸이 1이면 일단 ++1
+                if grid[y][x]== "1":
+                    t_c = t_c +1
+                    # 현재 칸 아틀란티스 해버리기
+                    self.eraser(grid, y, x, row, col)
+        return t_c
+    
+    def eraser(self, grid, y, x, row, col):            
+        # 경계선이면 리턴
+        if y<0 or x<0 or x>=col or y>=row:
+            return
+        # 바다라면 리턴
+        if grid[y][x]=="0":
+            return
+        # 그외 == 땅인 경우 모두 바다로 가라앉힘
+        grid[y][x]="0"
+        
+        # 상하좌우 연좌제 적용하기
+        self.eraser(grid, y-1, x, row, col)
+        self.eraser(grid, y+1, x, row, col)
+        self.eraser(grid, y, x-1, row, col)
+        self.eraser(grid, y, x+1, row, col)
